8bc4640e23f762718955d355c4286ba738631be6
AlfredoRoque <46386649+AlfredoRoque@users.noreply.github.com>
2021-09-27 16:44:37 -0500

    version 1.0

--- src/assets/js/mio.js
+++ src/assets/js/mio.js
@@ -2,7 +2,7 @@ addEventListener('load',()=>{
         var link = document.getElementById('cv');
         link.addEventListener('click',()=>{
                 link.setAttribute("target","_blank");
-                link.href = "https://drive.google.com/u/0/uc?id=1OL7nNRWKJ3tmtPCD2lQPuWE32PAHAu-4&export=download";
+                link.href = "https://drive.google.com/u/0/uc?id=1SCzWyOeMY_ug-SwVItDL3vQXR5w6odJS&export=download";
                 link.download = "C.V.pdf";
         })
 });

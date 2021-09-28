60c70757ea06615e8d3d66fc5a48532d5b4a5434
AlfredoRoque <46386649+AlfredoRoque@users.noreply.github.com>
2021-09-28 18:55:40 -0500

    version 1.0

--- src/assets/js/mio.js
+++ src/assets/js/mio.js
@@ -5,4 +5,18 @@ addEventListener('load',()=>{
                 link.href = "https://drive.google.com/u/0/uc?id=14vCT8m2qgVvG2d6OxyJPsVNT7YWUzz67&export=download";
                 link.download = "C.V.pdf";
         })
+
+        const lenguage = document.getElementById("lenguage");
+        if(lenguage!=null){
+           lenguage.addEventListener("change",(a)=>{
+           console.log(lenguage.value);
+           if(lenguage.value == 'es'){
+                lenguage.classList.remove("flag-en");
+                lenguage.classList.add("flag-es");
+           }else if(lenguage.value == 'en'){
+                lenguage.classList.remove("flag-es");
+                lenguage.classList.add("flag-en");
+           }
+        });
+}
 });

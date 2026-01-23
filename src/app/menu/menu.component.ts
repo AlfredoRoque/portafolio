8ebff74b16d50478b22fb1791a1ceb2a4ef5a5ed
Alfredo Roque <alfredoroque@Alfredos-MacBook-Air.local>
2026-01-23 02:01:07 -0600

    se ajustan urls para proyectos

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -20,7 +20,7 @@ export class MenuComponent implements OnInit {
   proyects:ProyectData[] = [
     new ProyectData('https://github.com/AlfredoRoque/webpag','https://webcamp-dev.herokuapp.com/', '../../assets/images/webcamp.PNG'),
     new ProyectData('https://github.com/AlfredoRoque/examen','https://examen-22.herokuapp.com/','../../assets/images/gestor_emp.PNG'),
-    new ProyectData('https://github.com/AlfredoRoque/pokedex','https://pokedex-inc.herokuapp.com/','../../assets/images/poke.PNG'),
+    new ProyectData('https://github.com/AlfredoRoque/pokedex','https://poke.rorideas.com/','../../assets/images/poke.PNG'),
     new ProyectData('https://admin-projects.herokuapp.com/login.php','https://github.com/AlfredoRoque/administradorTareas','../../assets/images/admin.PNG'),
     new ProyectData('https://github.com/AlfredoRoque/CryptoCoins/tree/main','https://crypto.rorideas.com/','../../assets/images/crypto.PNG'),
     new ProyectData('','https://siaf.puebla.gob.mx/','../../assets/images/siaf.PNG'),

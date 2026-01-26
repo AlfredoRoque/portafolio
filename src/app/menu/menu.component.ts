0248c5a53160fa298e2aea191eeb2ac1cb5082d2
Alfredo Roque <alfredoroque@Alfredos-MacBook-Air.local>
2026-01-25 20:11:24 -0600

    se actualizan rutas de proyectos y orden

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -19,11 +19,11 @@ export class MenuComponent implements OnInit {
 
   proyects:ProyectData[] = [
     new ProyectData('https://github.com/AlfredoRoque/webpag','https://landing-page.rorideas.com/', '../../assets/images/webcamp.PNG'),
-    new ProyectData('https://github.com/AlfredoRoque/examen','https://examen-22.herokuapp.com/','../../assets/images/gestor_emp.PNG'),
-    new ProyectData('https://github.com/AlfredoRoque/pokedex','https://poke.rorideas.com/','../../assets/images/poke.PNG'),
-    new ProyectData('https://github.com/AlfredoRoque/administradorTareas','https://task-administrator.rorideas.com/','../../assets/images/admin.PNG'),
-    new ProyectData('https://github.com/AlfredoRoque/CryptoCoins/tree/main','https://crypto.rorideas.com/','../../assets/images/crypto.PNG'),
     new ProyectData('','https://siaf.puebla.gob.mx/','../../assets/images/siaf.PNG'),
+    new ProyectData('https://github.com/AlfredoRoque/CryptoCoins/tree/main','https://crypto.rorideas.com/','../../assets/images/crypto.PNG'),
+    new ProyectData('https://github.com/AlfredoRoque/examen','https://cucas.pythonanywhere.com/','../../assets/images/gestor_emp.PNG'),
+    new ProyectData('https://github.com/AlfredoRoque/pokedex','https://poke.rorideas.com/','../../assets/images/poke.PNG'),
+    new ProyectData('https://github.com/AlfredoRoque/administradorTareas','https://task-administrator.rorideas.com/','../../assets/images/admin.PNG')
   ];
 
 

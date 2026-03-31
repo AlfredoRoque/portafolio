d27f3e87b57cb7102ac170dee9c29080023e0850
AlfredoRoque <alfredorokeromero@hotmail.com>
2026-03-30 18:14:51 -0600

    Se agrega proyecto de pasarela de pagos con stripe.

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -20,6 +20,7 @@ export class MenuComponent implements OnInit {
   proyects:ProyectData[] = [
     new ProyectData('https://github.com/AlfredoRoque/balances-dialisis-app','https://gestor-balances.rorideas.com/', '../../assets/images/balance-dialisis-front.png', 'Gestor de Balances de Diálisis'),
     new ProyectData('https://github.com/AlfredoRoque/gestor-balances-dialisis','https://gestor-balance-dialisis-production.up.railway.app/swagger-ui/index.html', '../../assets/images/balance-dialisis-back.png', 'API de Balances de Diálisis'),
+    new ProyectData('https://github.com/AlfredoRoque/payments','https://payments-production-4a56.up.railway.app/swagger-ui/index.html', '../../assets/images/stripe-payments.png', 'API de Pagos con Stripe'),
     new ProyectData('https://github.com/AlfredoRoque/email-sender','', '../../assets/images/email-sender.png', 'Consumer de Envío de Correos(kafka)'),
     new ProyectData('https://github.com/AlfredoRoque/webpag','https://landing-page.rorideas.com/', '../../assets/images/webcamp.PNG', 'Landing Page WebCamp'),
     new ProyectData('','https://siaf.puebla.gob.mx/','../../assets/images/siaf.PNG', 'SIAF Puebla'),
